228187df850147fb223fa90d7a962da9e4d18048
Rizwan Shivalli <rizshivalli@gmail.com>
2023-11-27 22:43:44 +0530

    RN screens fix

--- android/app/src/main/java/com/alohaadventures/MainActivity.java
+++ android/app/src/main/java/com/alohaadventures/MainActivity.java
@@ -1,6 +1,6 @@
-import android.os.Bundle;
 package com.alohaadventures;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
